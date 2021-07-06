29cd8762288d8ad25b353b7f3084068cfbe84afe
Emiel Legrand <emiel.legrand@gmail.com>
2021-07-06 15:20:28 +0200

    backup

--- .dep/mscLiterature.pdf.blg.d
+++
@@ -1,4 +0,0 @@
-mscLiterature.log : $(wildcard IEEEtranN.bst)
-mscLiterature.log : $(wildcard references.bib)
-mscLiterature.pdf : $(wildcard IEEEtranN.bst)
-mscLiterature.pdf : $(wildcard references.bib)
