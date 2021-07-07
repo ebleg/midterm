8ee2ec06d7a3e2810e7052fecfafc81b48d5bc3d
Emiel Legrand <emiel.legrand@gmail.com>
2021-07-08 00:13:19 +0200

    more progress

---
+++ .dep/mscLiterature.pdf.blg.d
@@ -0,0 +1,4 @@
+mscLiterature.log : $(wildcard IEEEtranN.bst)
+mscLiterature.log : $(wildcard references.bib)
+mscLiterature.pdf : $(wildcard IEEEtranN.bst)
+mscLiterature.pdf : $(wildcard references.bib)
